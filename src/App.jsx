097af39c30712a28dcfabc56dc3d9ebd42679bc8
iliass linboui <iliasslin02@gmail.com>
2024-02-21 15:18:35 +0100

    newAPP

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,32 @@ function handel(w){
           <div className='display'>
             <input value={value} readOnly />
           </div>
+          <div className='ss'> 
           <div>
             <input type='button' value="AC" onClick={(W)=>setValue('')} />
             <input type='button' value="DE" onClick={(W)=>setValue(value.slice(0,-1))} />
             <input type='button' value="." onClick={(W)=>setValue(value+W.target.value)} />
             <input type='button' value="/" onClick={(W)=>setValue(value+W.target.value)} />
+    
           </div>
           <div>
             <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="7" />
             <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="8" />
             <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="9" />
-            <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="1" />
-            <input type='button' value="*" onClick={(W)=>setValue(value+W.target.value)} />
+            <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="+" />
+            
           </div>
           <div>
             <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="4" />
             <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="5" />
             <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="6" />
+            <input type='button' value="*" onClick={(W)=>setValue(value+W.target.value)} />
+          </div>
+          <div>
+            <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="1" />
             <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="2" />
-
-            <input type='button' value="+" onClick={(W)=>setValue(value+W.target.value)} />
+            <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="3" />
+            <input onClick={(W)=>setValue(value+W.target.value)} type='button' value="-" />
           </div>
           <div>
             <input type='button' value="00" onClick={(W)=>setValue(value+W.target.value)} />
@@ -40,6 +46,7 @@ function handel(w){
             <input type='button' value="3" onClick={(W)=>setValue(value+W.target.value)} />
             <input type='button' value="=" onClick={e=>setValue(eval(value))}  />
           </div>
+          </div> 
         </form>
       </div>
     </div>
